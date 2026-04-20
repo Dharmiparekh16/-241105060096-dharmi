1089a9748785090622fdc1d7b9d4d1f09548518a
Dharmiparekh16 <dparekh1108@gmail.com>
2026-04-20 23:13:15 +0530

    Add files via upload

---
+++ Simple_Calculator.c
@@ -0,0 +1,10 @@
+Simple Calculator
+#include<stdio.h> 
+int main(){ 
+float a=10, b=5; 
+printf("Add: %.1f\n", a+b); 
+printf("Sub: %.1f\n", a-b); 
+printf("Mul: %.1f\n", a*b); 
+printf("Div: %.1f\n", a/b); 
+return 0;
+}
---
+++ ala_3_dharmi.py
@@ -0,0 +1,9 @@
+# Simple Calculator in Python
+
+a = 10
+b = 5
+
+print("Add: %.1f" % (a + b))
+print("Sub: %.1f" % (a - b))
+print("Mul: %.1f" % (a * b))
+print("Div: %.1f" % (a / b))
